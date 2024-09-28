5e77813e87935919e0d7842f25991f0fb20b8878
proksima <konstant.2015b@mail.ru>
2024-09-28 11:03:41 +0300

    Added one more test and redesigned code

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -3,31 +3,57 @@
 #include <filesystem>
 
 
-
 extern "C" {
 #include <parent.h>
 }
 
+template<size_t N>
+std::string join(const std::array<int, N> &elements, const std::string &delimiter) {
+    std::string result;
+    for (size_t i = 0; i < elements.size(); ++i) {
+        if (i > 0) {
+            result += delimiter;
+        }
+        result += std::to_string(elements[i]);
+    }
+    return result;
+}
 
-TEST(MockCout, test_1) {
+class Lab1Test : public testing::Test {
+protected:
     char *fileWithInput = (char *) "input.txt";
-    std::array<int, 5> input = {
-            4, 10, 5, 20, 31
-    };
 
-    std::array<int, 2> expectedOutput = {
-            4, 10
-    };
+    void TearDown() override {
+        auto removeIfExists = [](const char *path) {
+            if (std::filesystem::exists(path)) {
+                std::filesystem::remove(path);
+            }
+        };
 
-    {
+        removeIfExists(fileWithInput);
+    }
+
+    template<size_t N>
+    void WriteToFile(std::array<int, N> input) {
         auto inFile = std::ofstream(fileWithInput);
 
         for (const auto &line: input) {
             inFile << line << '\n';
         }
     }
+};
+
+
+TEST_F(Lab1Test, HandleSomePrimeNumbers) {
+    std::array<int, 5> input = {
+            4, 10, 5, 20, 31
+    };
+    WriteToFile(input);
+    std::array<int, 2> expectedOutput = {
+            4, 10
+    };
     char mybuffer[1024];
-    FILE * pFile = fmemopen(mybuffer, 1024, "w");
+    FILE *pFile = fmemopen(mybuffer, 1024, "w");
     std::swap(stdout, pFile);
 
     MainFunction(fileWithInput);
@@ -36,15 +62,26 @@ TEST(MockCout, test_1) {
     fclose(pFile);
     std::string capturedOutput(mybuffer);
     std::replace(capturedOutput.begin(), capturedOutput.end(), '\n', ' ');
-    capturedOutput.erase(capturedOutput.end()-1, capturedOutput.end());
-    ASSERT_EQ(capturedOutput, "4 10");
+    capturedOutput.erase(capturedOutput.end() - 1, capturedOutput.end());
+    ASSERT_EQ(capturedOutput, join(expectedOutput, " "));
+}
 
-
-    auto removeIfExists = [](const char *path) {
-        if (std::filesystem::exists(path)) {
-            std::filesystem::remove(path);
-        }
+TEST_F(Lab1Test, HandleAllPrimeNumbers) {
+    std::array<int, 4> input = {
+            3, 2, 5, 7
     };
+    WriteToFile(input);
+    std::array<int, 0> expectedOutput = {};
+    char mybuffer[1024];
+    FILE *pFile = fmemopen(mybuffer, 1024, "w");
+    std::swap(stdout, pFile);
 
-    removeIfExists(fileWithInput);
+    MainFunction(fileWithInput);
+
+    std::swap(stdout, pFile);
+    fclose(pFile);
+
+    std::string capturedOutput(mybuffer);
+    std::string expected = join(expectedOutput, " ");
+    EXPECT_EQ(capturedOutput, expected);
 }
